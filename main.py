ae340bef72697ef1881eb1e79e94505fbc35c1e6
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-08-08 21:05:28 +0530

    Ok

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import streamlit as st
-from modules import axioms, theorems, visuals
+from modules import axioms, euclidAxiomExplained, theorems, visuals
 
 st.set_page_config(page_title="Euclid Unfolded", layout="wide")
 
 st.title("📐 Euclid Unfolded: Geometry Reimagined")
 st.markdown("Explore axioms, prove theorems, and visualize classical geometry interactively.")
 
-tab1, tab2, tab3 = st.tabs(["Axiom Explorer", "Theorem Prover", "Visual Playground"])
+tab1, tab2, tab3,tab4 = st.tabs(["Axiom Explorer","Explanation to Axioms", "Theorem Prover", "Visual Playground"])
 
 with tab1:
     axioms.display_axioms()
-
+    
 with tab2:
+    axioms.display_axioms_explanation()
+    
+with tab3:
     theorems.prove_theorem()
 
-with tab3:
+with tab4:
     visuals.show_visuals()
